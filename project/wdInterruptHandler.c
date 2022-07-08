f0bc8946dd3e0ad74ed627b2686acea962014a83
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-07-08 12:33:53 -0600

    Aesthetic changes

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -1,8 +1,7 @@
 #include <msp430.h>
 #include "stateMachines.h"
 
-void
-__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+void __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
   if (++blink_count == 125) {
     state_advance();
