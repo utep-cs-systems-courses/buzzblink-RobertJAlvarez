814a1e75a993b6c86cdbcb4b857abdb983cf1894
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-07-08 12:13:33 -0600

    Delete uneccessary files

--- project/main.c
+++ project/main.c
@@ -8,6 +8,7 @@ int main(void) {
   switch_init();
   led_init();
   enableWDTInterrupts();  /* enable periodic interrupt */
+
   buzzer_init();
   buzzer_set_period(1000); //start buzzing! 2MHz/1000 = 2kHz
 
