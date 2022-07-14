cd947b2bf8f53fbcdc5b8dd07b759987fc8712eb
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-07-14 17:26:08 -0600

    Warning in object file, and executable doesn't behave as expected

--- project/select_SM-s.s
+++ project/select_SM-s.s
@@ -1,34 +1,35 @@
-  .arch msp430g2553
-  .p2align 1,0
-  .text
+    .arch msp430g2553
+    .p2align 1,0
+
+    .text
 jt: .word default
     .word case1
     .word case2
     .word case3
     .word case4
 
-  .global select_SM
-  .extern n_switch_down
+    .global select_SM
+    .extern n_switch_down
 
 select_SM:
-  ;; if (n_switch_down > 4) goto default
-  cmp #4, n_switch_down
-  jnc default
-  ;; PC = jt[n_switch_down]
-  mov &n_switch_down, r12
-  add r12, r12
-  mov jt(r12), r0
+    ;; if (n_switch_down > 4) goto default
+    cmp n_switch_down, #4 ; 4 - n_switch_down
+    jnc default           ; goto default if 4 < n_switch_down
+    ;; PC = jt[n_switch_down]
+    mov &n_switch_down, r12
+    add r12, r12
+    mov jt(r12), r0
 case1:
-  call #led_pattern_state_machine
-  jmp out
+    call #led_pattern_state_machine
+    jmp end
 case2:
-  call #dimming_state_machines
-  jmp out
+    call #dimming_state_machines
+    jmp end
 case3:
-  call #high_to_low_buzzer
-  jmp out
+    call #high_to_low_buzzer
+    jmp end
 case4:
 default:
-  call #off_leds_buzzer
-out:
-  pop r0
+    call #off_leds_buzzer
+end:
+    pop r0
