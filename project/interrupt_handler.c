6824e01074d6bb007b7c251fc6a9d8ca6f22486e
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-07-14 14:55:41 -0600

    __interrupt_vec(WDT_VECTOR WDT() function now select the state machine base on the bottom press by calling a function that is written in C and assembly

--- project/interrupt_handler.c
+++ project/interrupt_handler.c
@@ -1,6 +1,6 @@
 #include <msp430.h>
 #include "switches.h"
-#include "stateMachines.h"
+#include "select_SM.h"
 
 // Switch on S2
 void __interrupt_vec(PORT2_VECTOR) Port_2(){
@@ -11,19 +11,6 @@ void __interrupt_vec(PORT2_VECTOR) Port_2(){
 }
 
 void __interrupt_vec(WDT_VECTOR) WDT(){	//250 interrupts/sec
-  switch (n_switch_down) {
-  case 1:   //Alternate between toggling red & green
-    led_pattern_state_machine();
-    break;
-  case 2:   //Dim green light, toggle red light and update buz
-    dimming_state_machines();
-    break;
-  case 3:
-    high_to_low_buzzer();
-    break;
-  case 4:
-  default:  //Turn leds off and stop buzzer
-    off_leds_buzzer();
-  }
+  select_SM();
 }
 
---
+++ project/select_SM-c.c
@@ -0,0 +1,21 @@
+#include "switches.h"
+#include "stateMachines.h"
+#include "select_SM.h"
+
+void select_SM()
+{
+  switch (n_switch_down) {
+  case 1:   //Alternate between toggling red & green
+    led_pattern_state_machine();
+    break;
+  case 2:   //Dim green light, toggle red light and update buzz
+    dimming_state_machines();
+    break;
+  case 3:   //Go from a high to low frequency at different rates
+    high_to_low_buzzer();
+    break;
+  case 4:   //Turn leds off and stop buzzer
+  default:
+    off_leds_buzzer();
+  }
+}
